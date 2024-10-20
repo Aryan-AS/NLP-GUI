dca5e173cefaff382e8ae98c6304fee3264b5724
Aryan-AS <89517035+Aryan-AS@users.noreply.github.com>
2024-10-20 20:17:22 +0530

    Update myapi.py

--- myapi.py
+++ myapi.py
@@ -1,46 +1,27 @@
-# import requests
-
-# class API:
-#  def sentiment_analysis(self,text):
-#   url = "https://twinword-sentiment-analysis.p.rapidapi.com/analyze/"
-
-  
-#   querystring = text
-
-#   headers = {
-# 	"x-rapidapi-key": "e595913211msh89f5988df939177p1a0427jsn8076c2e4d727",
-# 	"x-rapidapi-host": "twinword-sentiment-analysis.p.rapidapi.com"
-# }
-
-#   response = requests.get(url, headers=headers, params=querystring)
-#   print(response)
-
-# import paralleldots
-
-# class API:
-#     def __init__(self):
-#         # Replace with your actual API key from ParallelDots
-#         paralleldots.set_api_key("XXYd2KTC61CaOv2owJYEqGJAEg9HtotX")
-    
-#     def sentiment_analysis(self, text):
-#         # Call Paralleldots sentiment analysis API
-#         response = paralleldots.sentiment(text)
-#         return response
-
 import requests
 
 class API:
- def sentiment_analysis(self, cute):
-  url = "https://api.apilayer.com/sentiment/analysis"
+  def sentiment_analysis(self,cute):
 
-  payload = cute
-  headers= {
-   "apikey": "XXYd2KTC61CaOv2owJYEqGJAEg9HtotX"
-}
-
-  response = requests.request("POST", url, headers=headers, data = payload)
-
-  status_code = response.status_code
-  result = response.text
-  return result
-
+   url = "https://sentiment-analysis9.p.rapidapi.com/sentiment"
+   
+   payload = [
+   	{
+   		"id": "1",
+   		"language": "en",
+   		"text": cute
+   	}
+   ]
+   headers = {
+   	"x-rapidapi-key": "9136cbe998msh78b7572a1d66429p165b86jsn536cddaba829",
+   	"x-rapidapi-host": "sentiment-analysis9.p.rapidapi.com",
+   	"Content-Type": "application/json",
+   	"Accept": "application/json"
+   }
+   
+   response = requests.post(url, json=payload, headers=headers)
+   
+   result =response.json()
+   return result
+    
+   
